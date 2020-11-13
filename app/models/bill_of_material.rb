fd5973e4d3163c70b6207e94183fed4af1b2f5e4
adjidedjo <adji.el.djo@gmail.com>
2020-11-13 10:09:44 +0700

    fix

--- app/models/bill_of_material.rb
+++ app/models/bill_of_material.rb
@@ -6,11 +6,13 @@ class BillOfMaterial < ActiveRecord::Base
     (2..spreadsheet.last_row).each do |i|
       row = Hash[[header, spreadsheet.row(i)].transpose]
       short_item = JdeItemMaster.get_short_item(row["item_number"])
-      item << JdeBillOfMaterial.count_bom_cunsumtion(short_item.imitm.to_i, row["qty"], userbp)
+      plan = short_item.imprp4.strip == 'KB' ? '2' : '1'
+      userbpplan = userbp + plan
+      item << JdeBillOfMaterial.count_bom_cunsumtion(short_item.imitm.to_i, row["qty"], userbpplan)
     end
     
     sum_items = item.flatten.group_by(&:ixlitm).map{|key, val|
-      {:ixlitm => key, :imdsc => val.first.imdsc1,:ixum => val.first.ixum, :ixmmcu => val.first.ixmmcu,
+      {:ixlitm => key, :imdsc => val.first.imdsc1.strip+' '+val.first.imdsc1.strip,:ixum => val.first.ixum, :ixmmcu => val.first.ixmmcu,
         :total => val.sum{|v| v.ixqnty.to_f}}}
     return sum_items
   end
